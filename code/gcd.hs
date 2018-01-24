0024ab1a90c70b645f000cd11dda976aebca9efb
Nan Xiao <nan@chinadtrace.org>
2018-01-24 09:09:51 +0800

    Update gcd.hs

--- code/gcd.hs
+++ code/gcd.hs
@@ -5,6 +5,5 @@ gcd' a b
     | b < 0 = gcd' a (-b)
     | a < b = gcd' b a
     | otherwise = let r = a `mod` b
-                  in
-                      case r of 0 -> b
-                                _ -> gcd' b r
+                  in case r of 0 -> b
+                               _ -> gcd' b r
